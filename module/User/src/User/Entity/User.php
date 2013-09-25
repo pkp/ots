09619c8398010b295647e47eb0bad3595ca09fa6
Michael Thessel <der.thes@gmail.com>
2013-09-24 17:27:43 -1000

    Added user attributes

--- module/User/src/User/Entity/User.php
+++ module/User/src/User/Entity/User.php
@@ -17,7 +17,56 @@ class User
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
-     *
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=false, unique=true)
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=32, nullable=false)
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="string", length=23, nullable=false)
+     */
+    protected $passwordSalt;
+
+    /**
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    protected $registrationDate;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $lastLogin;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=false)
+     */
+    protected $userLevel;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setRegistrationDate()
+    {
+        if (!$this->registrationDate) {
+            $this->registrationDate = time();
+        }
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setPasswordSalt()
+    {
+        if (!$this->passwordSalt) {
+            $this->passwordSalt = uniqid('', true);
+        }
+    }
 }
