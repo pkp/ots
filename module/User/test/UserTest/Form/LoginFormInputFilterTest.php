0e7a79c6cfa3496c3c46bd14e6dfaad92ba0df71
Michael Thessel <der.thes@gmail.com>
2013-10-03 13:34:52 -1000

    Made unittest instance of Xmlps\UnitTest\ModelTest

--- module/User/test/UserTest/Form/LoginFormInputFilterTest.php
+++ module/User/test/UserTest/Form/LoginFormInputFilterTest.php
@@ -2,12 +2,15 @@
 
 namespace UserTest\Form;
 
-use PHPUnit_Framework_TestCase;
+use Xmlps\UnitTest\ModelTest;
 use User\Form\LoginFormInputFilter;
 
-class LoginFormInputFilterTest extends PHPUnit_Framework_TestCase
+class LoginFormInputFilterTest extends ModelTest
 {
-    protected $traceError = true;
+    protected $userDAO;
+
+    protected $testUserEmail = 'unittestuser@example.com';
+    protected $testUserPassword = '5cebb03d702827bb9e25b38b06910fa5';
 
     /**
      * Initialize the test
@@ -16,8 +19,12 @@ class LoginFormInputFilterTest extends PHPUnit_Framework_TestCase
      */
     public function setUp()
     {
+        parent::setUp();
+
         $this->inputFilter = new LoginFormInputFilter;
         $this->inputFilter = $this->inputFilter->getInputFilter();
+
+        $this->userDAO = $this->sm->get('UserDAO');
     }
 
     /**
@@ -31,24 +38,37 @@ class LoginFormInputFilterTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($this->inputFilter->has('email'));
         $this->assertTrue($this->inputFilter->has('password'));
 
+        $data = array(
+            'email' => $this->testUserEmail,
+            'password' => $this->testUserPassword,
+        );
+
+        // Correct data
+        $this->inputFilter->setData($data);
+        $this->assertTrue($this->inputFilter->isValid());
+
         // Not empty email
-        $this->inputFilter->setData(array('email' => '', 'password' => 'password'));
+        $testData = $data;
+        $testData['email'] = '';
+        $this->inputFilter->setData($testData);
         $this->assertFalse($this->inputFilter->isValid());
 
         // Not empty password
-        $this->inputFilter->setData(array('email' => 'blim@bla.com', 'password' => ''));
+        $testData = $data;
+        $testData['password'] = '';
+        $this->inputFilter->setData($testData);
         $this->assertFalse($this->inputFilter->isValid());
 
         // Password length
-        $this->inputFilter->setData(array('email' => 'blim@bla.com', 'password' => 'short'));
+        $testData = $data;
+        $testData['password'] = 'short';
+        $this->inputFilter->setData($testData);
         $this->assertFalse($this->inputFilter->isValid());
 
         // Email address
-        $this->inputFilter->setData(array('email' => 'invalid email address', 'password' => '1234567'));
+        $testData = $data;
+        $testData['email'] = 'invalid email address';
+        $this->inputFilter->setData($testData);
         $this->assertFalse($this->inputFilter->isValid());
-
-        // Correct data
-        $this->inputFilter->setData(array('email' => 'blim@bla.com', 'password' => '1234567'));
-        $this->assertTrue($this->inputFilter->isValid());
     }
 }
