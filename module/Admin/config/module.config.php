dfc0bf0bfb02c48b5771c84a59c26e87af0be903
Michael Thessel <der.thes@gmail.com>
2013-10-18 10:47:19 -1000

    Removed doctrine entity configuration from Admin module

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -51,22 +51,6 @@ return array(
             'admin' => __DIR__ . '/../view'
         ),
     ),
-    'doctrine' => array(
-        'driver' => array(
-            __NAMESPACE__ . '_driver' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(
-                    __DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'
-                ),
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-                ),
-            ),
-        ),
-    ),
 
     'acl' => array(
         'resources' => array(
