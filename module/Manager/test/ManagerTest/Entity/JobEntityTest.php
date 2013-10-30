2a25c6c483cce638f6a0146b07f97f06f406b3c9
Michael Thessel <der.thes@gmail.com>
2013-10-30 13:16:48 -1000

    Added unit test for job entity

---
+++ module/Manager/test/ManagerTest/Entity/JobEntityTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ManagerTest\Entity;
+
+use PHPUnit_Framework_TestCase;
+use Xmlps\UnitTest\ModelTest;
+use Manager\Entity\Job;
+
+class JobEntityTest extends ModelTest
+{
+    protected $jobDAO;
+
+    /**
+     * Initialize the test
+     *
+     * @return void
+     */
+    public function setUp() {
+        parent::setUp();
+
+        $this->jobDAO = $this->sm->get('Manager\Model\DAO\JobDAO');
+    }
+
+    /**
+     * Test if the creation date is initialized correctly
+     *
+     * @return void
+     */
+    public function testCreationDateInitialization()
+    {
+        $job = $this->jobDAO->getInstance();
+
+        $this->assertNull($job->creationDate);
+        $job->initCreationDate();
+        $this->assertTrue(is_int($job->creationDate));
+        $this->assertTrue($job->creationDate <= time());
+    }
+
+    /**
+     * Test if the status is initialized correctly
+     *
+     * @return void
+     */
+    public function testStatusInitialization()
+    {
+        $job = $this->jobDAO->getInstance();
+        $this->assertNull($job->status);
+        $job->initStatus();
+        $this->assertSame($job->status, JOB_STATUS_PENDING);
+    }
+}
