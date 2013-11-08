52d2c2e0709e3ae666065c5c0b1abb3c6467afd4
Michael Thessel <der.thes@gmail.com>
2013-11-07 16:39:16 -1000

    Moved conversion configuration to autoload config files

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -38,5 +38,12 @@ return array(
     ),
     'notification' => array(
         'adminEmail' => 'axfelix@gmail.com',
-    )
+    ),
+    'conversion' => array(
+        'docx' => array(
+            'unoconv' => array(
+                'command' => '/opt/libreoffice4.1/program/python vendor/dagwieers/unoconv/unoconv',
+            ),
+        ),
+    ),
 );
--- module/DocxConversion/config/module.config.php
+++ module/DocxConversion/config/module.config.php
@@ -1,12 +1,2 @@
 <?php
-namespace DocxConversion;
-
-return array(
-    'conversion' => array(
-        'docx' => array(
-            'unoconv' => array(
-                'command' => '/opt/libreoffice4.1/program/python vendor/dagwieers/unoconv/unoconv',
-            ),
-        ),
-    ),
-);
+return array();
