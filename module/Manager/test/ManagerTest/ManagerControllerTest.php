3dbb670adc03c30f41c32fd4e301c4ac1e3e0fb1
Michael Thessel <der.thes@gmail.com>
2013-11-12 16:01:35 -1000

    Added unit tests for manager list, details and download actions

--- module/Manager/test/ManagerTest/ManagerControllerTest.php
+++ module/Manager/test/ManagerTest/ManagerControllerTest.php
@@ -2,18 +2,25 @@
 
 namespace ManagerTest\Controller;
 
+use Manager\Entity\Job;
 use Xmlps\UnitTest\ControllerTest;
 
 class ManagerControllerTest extends ControllerTest
 {
     protected $userDAO;
+    protected $jobDAO;
+    protected $documentDAO;
 
     protected $testUserEmail = 'unittestuser@example.com';
     protected $testUserPassword = '5cebb03d702827bb9e25b38b06910fa5';
     protected $testUserRole = 'member';
     protected $testUserActive = true;
 
-    protected $testUser;
+    protected $testFile = '/tmp/UNITTEST_manager.txt';
+
+    protected $user;
+    protected $job;
+    protected $document;
 
     /**
      * Set up the controller test
@@ -25,6 +32,8 @@ class ManagerControllerTest extends ControllerTest
         parent::setUp();
 
         $this->userDAO = $this->sm->get('User\Model\DAO\UserDAO');
+        $this->jobDAO = $this->sm->get('Manager\Model\DAO\JobDAO');
+        $this->documentDAO = $this->sm->get('Manager\Model\DAO\DocumentDAO');
 
         $this->resetTestData();
     }
@@ -63,6 +72,93 @@ class ManagerControllerTest extends ControllerTest
     }
 
     /**
+     * Test if the list action cannot be accessed by guests
+     *
+     * @return void
+     */
+    public function testListActionCannotBeAccessedLoggedOut()
+    {
+        $this->dispatch('/manager/list');
+        $this->assertResponseStatusCode(403);
+    }
+
+    /**
+     * Test if the list action can be accessed by logged in users
+     *
+     * @return void
+     */
+    public function testListActionCanBeAccessedMember()
+    {
+        $this->mockLogin($this->user);
+        $this->dispatch('/manager/list');
+        $this->assertResponseStatusCode(200);
+    }
+
+    /**
+     * Test if the details action cannot be accessed by guests
+     *
+     * @return void
+     */
+    public function testDetailsActionCannotBeAccessedLoggedOut()
+    {
+        $this->dispatch('/manager/details/id/' . $this->job->id);
+        $this->assertResponseStatusCode(403);
+    }
+
+    /**
+     * Test if the details action can be accessed by logged in users
+     *
+     * @return void
+     */
+    public function testDetailsActionCanBeAccessedMember()
+    {
+        $this->mockLogin($this->user);
+        $this->dispatch('/manager/details/id/' . $this->job->id);
+        $this->assertResponseStatusCode(200);
+    }
+
+    /**
+     * Test if the download action cannot be accessed by guests
+     *
+     * @return void
+     */
+    public function testDownloadActionCannotBeAccessedLoggedOut()
+    {
+        $this->dispatch('/manager/download/id/' . $this->document->id);
+        $this->assertResponseStatusCode(403);
+    }
+
+    /**
+     * Test if the download action works correctly
+     *
+     * @return void
+     */
+    public function testDownloadAction()
+    {
+        $this->mockLogin($this->user);
+        $this->dispatch('/manager/download/id/' . $this->document->id);
+        $this->assertResponseStatusCode(200);
+
+        $headers = $this->getResponse()->getHeaders();
+
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $this->assertSame(
+            $headers->get('Content-Type')->value,
+            finfo_file($finfo, $this->testFile)
+        );
+
+        $this->assertSame(
+            $headers->get('Content-Disposition')->value,
+            'attachment; filename="' . basename($this->testFile) . '"'
+        );
+
+        $this->assertSame(
+            (int) $headers->get('Content-Length')->value,
+            filesize($this->testFile)
+        );
+    }
+
+    /**
      * Creates test data for this test
      *
      * @return void
@@ -76,6 +172,20 @@ class ManagerControllerTest extends ControllerTest
         $this->user->role = $this->testUserRole;
         $this->user->active = $this->testUserActive;
         $this->userDAO->save($this->user);
+
+        $this->job = $this->jobDAO->getInstance();
+        $this->job->user = $this->user;
+
+        touch($this->testFile);
+        file_put_contents($this->testFile, rand());
+
+        $this->document = $this->documentDAO->getInstance();
+        $this->document->path = $this->testFile;
+        $this->document->conversionStage = JOB_CONVERSION_STAGE_UNCONVERTED;
+        $this->document->job = $this->job;
+        $this->job->documents[] = $this->document;
+
+        $this->jobDAO->save($this->job);
     }
 
     /**
@@ -89,5 +199,7 @@ class ManagerControllerTest extends ControllerTest
         if ($user = $this->userDAO->findOneBy(array('email' => $this->testUserEmail))) {
             $this->userDAO->remove($user);
         }
+
+        @unlink($this->testFile);
     }
 }
