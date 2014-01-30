b304695f81f8548eb34dd51f765ca6f5a339cd41
Michael Thessel <michael.thessel@gmx.de>
2014-01-29 15:40:35 -1000

    Refactored ReferencesJobTest test data creation

--- module/ReferencesConversion/test/ReferencesConversionTest/Model/Queue/ReferencesJobTest.php
+++ module/ReferencesConversion/test/ReferencesConversionTest/Model/Queue/ReferencesJobTest.php
@@ -9,21 +9,13 @@ use ReferencesConversion\Model\Queue\Job\ReferencesJob;
 class ReferencesJobTest extends ModelTest
 {
     protected $document;
-    protected $documentDAO;
     protected $job;
-    protected $jobDAO;
     protected $user;
-    protected $userDAO;
 
     protected $referencesJob;
 
     protected $testAsset = 'module/ReferencesConversion/test/assets/document.xml';
     protected $testFile = '/tmp/UNITTEST_document.xml';
-
-    protected $testUserEmail = 'unittestuser@example.com';
-    protected $testUserPassword = '5cebb03d702827bb9e25b38b06910fa5';
-    protected $testUserRole = 'member';
-    protected $testUserActive = true;
 
     /**
      * Initialize the test
@@ -32,10 +24,6 @@ class ReferencesJobTest extends ModelTest
      */
     public function setUp() {
         parent::setUp();
-
-        $this->userDAO = $this->sm->get('User\Model\DAO\UserDAO');
-        $this->jobDAO = $this->sm->get('Manager\Model\DAO\JobDAO');
-        $this->documentDAO = $this->sm->get('Manager\Model\DAO\DocumentDAO');
 
         $this->referencesJob = new ReferencesJob;
         $this->referencesJob->setServiceLocator($this->sm);
@@ -65,28 +53,30 @@ class ReferencesJobTest extends ModelTest
      * @return void
      */
     protected function createTestData() {
-        copy($this->testAsset, $this->testFile);
+        @copy($this->testAsset, $this->testFile);
 
-        $this->user = $this->userDAO->getInstance();
-        $this->user->email = $this->testUserEmail;
-        $this->user->password = $this->testUserPassword;
-        $this->user->role = $this->testUserRole;
-        $this->user->active = $this->testUserActive;
-        $this->userDAO->save($this->user);
+        // Create test user
+        $this->user = $this->createTestUser();
 
-        $this->job = $this->jobDAO->getInstance();
-        $this->job->user = $this->user;
-        $this->job->conversionStage = JOB_CONVERSION_STAGE_NLMXML;
-        $this->job->setCitationStyleFileByTitle('Acta Ophthalmologica');
+        // Create test job
+        $this->job = $this->createTestJob(
+            array(
+                'user' => $this->user,
+                'conversionStage' => 2, // JOB_CONVERSION_STAGE_NLMXML
+            )
+        );
 
-        $this->document = $this->documentDAO->getInstance();
-        $this->document->job = $this->job;
-
-        $this->document->path = $this->testFile;
-        $this->document->conversionStage = $this->job->conversionStage;
-
+        // Create test document
+        $this->document = $this->createTestDocument(
+            array(
+                'job' => $this->job,
+                'path' => $this->testFile,
+                'conversionStage' => $this->job->conversionStage,
+            )
+        );
         $this->job->documents[] = $this->document;
-        $this->jobDAO->save($this->job);
+
+        $this->getJobDAO()->save($this->job);
     }
 
     /**
@@ -96,9 +86,7 @@ class ReferencesJobTest extends ModelTest
      */
     protected function cleanTestData()
     {
-        $user = $this->userDAO->findOneBy(array('email' => $this->testUserEmail));
-        if (!$user) return;
-        $this->userDAO->remove($user);
+        $this->deleteTestUser();
 
         @unlink($this->testFile);
     }
