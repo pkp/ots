d40422ef9ff8423f6960637439de2ab574b05b68
Michael Thessel <michael.thessel@gmx.de>
2014-01-29 15:28:44 -1000

    Refactored PdfJobTest test data creation

--- module/PdfConversion/test/PdfConversionTest/Model/Queue/PdfJobTest.php
+++ module/PdfConversion/test/PdfConversionTest/Model/Queue/PdfJobTest.php
@@ -7,23 +7,14 @@ use PdfConversion\Model\Queue\Job\PdfJob;
 
 class PdfJobTest extends ModelTest
 {
-    protected $document;
-
-    protected $documentDAO;
     protected $job;
-    protected $jobDAO;
     protected $user;
-    protected $userDAO;
+    protected $document;
 
     protected $pdfJob;
 
     protected $testAsset = 'module/PdfConversion/test/assets/html.zip';
     protected $testFile = '/tmp/UNITTEST_pdf_document.pdf';
-
-    protected $testUserEmail = 'unittestuser@example.com';
-    protected $testUserPassword = '5cebb03d702827bb9e25b38b06910fa5';
-    protected $testUserRole = 'member';
-    protected $testUserActive = true;
 
     /**
      * Initialize the test
@@ -67,25 +58,28 @@ class PdfJobTest extends ModelTest
     protected function createTestData() {
         @copy($this->testAsset, $this->testFile);
 
-        $this->user = $this->userDAO->getInstance();
-        $this->user->email = $this->testUserEmail;
-        $this->user->password = $this->testUserPassword;
-        $this->user->role = $this->testUserRole;
-        $this->user->active = $this->testUserActive;
-        $this->userDAO->save($this->user);
+        // Create test user
+        $this->user = $this->createTestUser();
 
-        $this->job = $this->jobDAO->getInstance();
-        $this->job->user = $this->user;
-        $this->job->conversionStage = JOB_CONVERSION_STAGE_CITATIONSTYLE;
-        $this->job->setCitationStyleFileByTitle('Acta Ophthalmologica');
+        // Create test job
+        $this->job = $this->createTestJob(
+            array(
+                'user' => $this->user,
+                'conversionStage' => 7, // JOB_CONVERSION_STAGE_CITATIONSTYLE
+            )
+        );
 
-        $this->document = $this->documentDAO->getInstance();
-        $this->document->job = $this->job;
-        $this->document->path = $this->testFile;
-        $this->document->conversionStage = $this->job->conversionStage;
+        // Create test document
+        $this->document = $this->createTestDocument(
+            array(
+                'job' => $this->job,
+                'path' => $this->testFile,
+                'conversionStage' => $this->job->conversionStage,
+            )
+        );
         $this->job->documents[] = $this->document;
 
-        $this->jobDAO->save($this->job);
+        $this->getJobDAO()->save($this->job);
     }
 
     /**
@@ -95,9 +89,7 @@ class PdfJobTest extends ModelTest
      */
     protected function cleanTestData()
     {
-        $user = $this->userDAO->findOneBy(array('email' => $this->testUserEmail));
-        if (!$user) return;
-        $this->userDAO->remove($user);
+        $this->deleteTestUser();
 
         @unlink($this->testFile);
     }
