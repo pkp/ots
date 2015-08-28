5d72ec474ac17817e683d4914da4213539977533
Christopher R. Maden <crism@maden.org>
2015-08-27 23:59:16 -0500

    Issue #19: Allowing unoconv to run as homeless user.

--- module/DocxConversion/src/DocxConversion/Model/Converter/Unoconv.php
+++ module/DocxConversion/src/DocxConversion/Model/Converter/Unoconv.php
@@ -98,8 +98,9 @@ class Unoconv extends AbstractConverter
     {
         $command = new Command;
 
-        // Set the base command
-        $command->setCommand($this->config['command']);
+        // Set the base command.  If HOME is not set to a writeable
+        // directory, unoconv won’t work.
+        $command->setCommand('HOME=/tmp ' . $this->config['command']);
 
         // Add verbosity switch
         if ($this->verbose) $command->addSwitch('-vvv');
