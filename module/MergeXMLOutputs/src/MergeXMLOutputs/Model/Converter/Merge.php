d8188284ec4373fb3985d0f0bd9868b343ab1a33
Alex Garnett <axfelix@gmail.com>
2018-02-08 13:06:04 -0800

    cleaned up unneeded loop

--- module/MergeXMLOutputs/src/MergeXMLOutputs/Model/Converter/Merge.php
+++ module/MergeXMLOutputs/src/MergeXMLOutputs/Model/Converter/Merge.php
@@ -212,23 +212,17 @@ class Merge extends AbstractConverter
         }
 
         // Bring aff out of contrib-group to make Texture happy
-        $k = 0;
         $articleMetaElements = $meTypesetDom->getElementsByTagName('article-meta');
-        // This shouldn't need to be a loop as there's only one articleMeta 
-        // but I copied it from the above block and don't recognize the syntax
-        foreach ($articleMetaElements as $articleMeta) {
-          $articleMetaItem = $articleMetaElements->item($k);
-          $contribGroup = $articleMetaItem->getElementsByTagName('contrib-group');
-          $children = [];
-          foreach ($contribGroup[0]->childNodes as $gNode) {
-            if ($gNode->nodeType == XML_ELEMENT_NODE and $gNode->localName == "aff") {
-              $children[] = $gNode;
-            }
+        $articleMetaItem = $articleMetaElements->item(0);
+        $contribGroup = $articleMetaItem->getElementsByTagName('contrib-group');
+        $children = [];
+        foreach ($contribGroup[0]->childNodes as $gNode) {
+          if ($gNode->nodeType == XML_ELEMENT_NODE and $gNode->localName == "aff") {
+            $children[] = $gNode;
           }
-          foreach ($children as $child) {
-            $articleMetaItem->appendChild($child);
-          }
-          $k++;
+        }
+        foreach ($children as $child) {
+          $articleMetaItem->appendChild($child);
         }
 
         $newXml = $meTypesetDom->saveXML();
